2e4b0bf85db9f8bb97022b592090d1b74d6a4638
Jozsef88 <49954645+Jozsef88@users.noreply.github.com>
2020-04-01 22:34:13 +0100

    Add files via upload

---
+++ build/precache-manifest.158d3d48ba795e606450c90945210cf2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dad69eb85f91c060fd5976cb510330c",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e41a6ebd2e34e160d139",
+    "url": "/weatherapp/static/css/main.2356a00a.chunk.css"
+  },
+  {
+    "revision": "5b9c76e3532d49265a2f",
+    "url": "/weatherapp/static/js/2.6ba3ac9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.6ba3ac9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41a6ebd2e34e160d139",
+    "url": "/weatherapp/static/js/main.e0bf78c6.chunk.js"
+  },
+  {
+    "revision": "ff907cfbbb256dae02f8",
+    "url": "/weatherapp/static/js/runtime-main.f16cac52.js"
+  },
+  {
+    "revision": "9b44a343acbd2a61619857e5002ceda4",
+    "url": "/weatherapp/static/media/bg-shape.9b44a343.svg"
+  },
+  {
+    "revision": "412dea686f9bfd2f1b5cf7f698d3e334",
+    "url": "/weatherapp/static/media/pic-1.412dea68.jpg"
+  },
+  {
+    "revision": "236ccd820e72c9793a444cb25a564593",
+    "url": "/weatherapp/static/media/pic-2.236ccd82.jpg"
+  },
+  {
+    "revision": "916aea4e49d55bc03b204c40181c213b",
+    "url": "/weatherapp/static/media/snow-icon.916aea4e.svg"
+  },
+  {
+    "revision": "ee97e668672445b8300aab7721757f82",
+    "url": "/weatherapp/static/media/sun-icon.ee97e668.svg"
+  }
+]);
